7ef918ed8b4d3d55b80012987bb70b26408e9ad4
unknown <ryansgrt23@gmail.com>
2020-11-21 11:31:51 +0700

    add icon header anymore

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -48,7 +48,7 @@ const CartScreen = ({ match, location, history }) => {
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>Rp.{item.price}</Col>
                   <Col md={2}>
                     <Form.Control
                       as='select'
